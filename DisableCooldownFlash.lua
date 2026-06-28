a4cde1f907cd7da5ffb92cb3d8b965996d558647
fab <fabiopalma2504@gmail.com>
2026-06-28 19:12:59 +0200

    fix: made a simpler, more stable version

--- DisableCooldownFlash.lua
+++ DisableCooldownFlash.lua
@@ -1,27 +1,7 @@
-local function DisableCooldownFlash(cooldown)
-    if not cooldown then
-        return
-    end
+local CooldownMixin = getmetatable(ActionButton1Cooldown).__index
 
-    if cooldown.SetDrawBling and cooldown.GetObjectType and cooldown:GetObjectType() == "Cooldown" then
-        pcall(cooldown.SetDrawBling, cooldown, false)
-    end
-end
-
-hooksecurefunc("CooldownFrame_Set", function(cooldown)
-    DisableCooldownFlash(cooldown)
-end)
-
-local frame = CreateFrame("Frame")
-frame:RegisterEvent("PLAYER_LOGIN")
-frame:SetScript("OnEvent", function()
-    for i = 1, 10 do
-        local bar = _G["BT4Bar" .. i]
-
-        if bar and bar.buttons then
-            for _, button in pairs(bar.buttons) do
-                DisableCooldownFlash(button.cooldown)
-            end
-        end
+hooksecurefunc(CooldownMixin, "SetCooldown", function(self)
+    if self.SetDrawBling then
+        self:SetDrawBling(false)
     end
 end)
